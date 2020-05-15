da19258bb5d15482817c4b70953f5026313ca590
alexandradew <alexandradew@protonmail.com>
2020-05-15 04:12:24 -0300

    menu fix

--- js/app.js
+++ js/app.js
@@ -9,14 +9,16 @@ $(function () {
 });
 
 function changeColor() {
-    $('#nav').css("border-bottom", "1px solid #9b59b6").removeClass("navbar-dark").addClass("menu-white");
+    $('#nav').css("border-bottom", "1px solid #9b59b6").addClass("menu-white");
     $('#nav').css("background-color", "#FFF");
     $('#nav .btn').addClass("btn-default");
     $('.navbar-brand').addClass("brand");
+    $('.active').addClass("active-white");
 
     $('#dropdown-menu').removeClass("bg-transparent").addClass("dropdown-menu-white").css("background-color", "#FFF")
     $('#dropdown-menu a').removeClass("text-light").css("color", "#9b59b6")
 
     $('.navbar-toggler').removeClass("bg-transparent")
     $('.navbar-toggler').css("background-color", "#9b59b6")
+    $('.navbar-toggler').css("color", "#FFF")
 }
